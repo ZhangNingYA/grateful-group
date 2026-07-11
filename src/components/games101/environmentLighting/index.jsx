f1dfce8d270d5943e98ded80b7ec3f7abf36ae8a
cyantere <vuholosu@gmail.com>
2026-07-11 16:04:19 +0800

    refactor(works): remove legacy graphics archive

--- src/components/games101/environmentLighting/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as CubeMapVsSphereMap } from './CubeMapVsSphereMap.jsx'
-export { default as IBLIntegrationDemo } from './IBLIntegrationDemo.jsx'
-export { default as SphericalHarmonicsViz } from './SphericalHarmonicsViz.jsx'
-export { default as EnvironmentLightingComparison } from './EnvironmentLightingComparison.jsx'
