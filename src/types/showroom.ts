7fb98b0417ea80915e9263a41007dd837b4dca41
cyantere <vuholosu@gmail.com>
2026-08-28 21:23:47 +0800

    Remove unused demo code and assets

--- src/types/showroom.ts
+++
@@ -1,8 +0,0 @@
-export type ShowroomModelId = 'watch' | 'speaker' | 'lamp';
-
-export interface ShowroomModelDefinition {
-  id: ShowroomModelId;
-  name: string;
-  summary: string;
-  sizeHint: string;
-}
